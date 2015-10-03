b5f3a341cd4707ba2cd5842d9a58a4f3fa719a7f
shvagahb <biancaaidass@gmail.com>
2015-10-03 16:44:09 -0400

    woah

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-
+// var appBtn = document.getElementById("appBtn");
 var numOfBackgrounds = 11;
 	var n = Math.floor((Math.random() * numOfBackgrounds) + 1);
 	document.body.style.backgroundImage = "url('bg/"+n+".jpg')";
 
-	appBtn.addEventListener("click", function(){
-		chrome.tabs.update({
-            url:'chrome://apps'
-        });
-	});
+	// appBtn.addEventListener("click", function(){
+	// 	chrome.tabs.update({
+ //            url:'chrome://apps'
+ //        });
+	// });
 
 
 
