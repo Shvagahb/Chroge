da6b4a4062ad562b9bd72b5a348cd78d7101d6e7
unknown <bgidas@WC7KHM7M1.net.dos.state.fl.us>
2015-10-09 12:47:37 -0400

    quote generator

--- index.js
+++ index.js
@@ -12,6 +12,39 @@ var numOfBackgrounds = 11;
 
 
 
+
+var Quotation=new Array() // do not change this!
+
+// Set up the quotations to be shown, below.
+// To add more quotations, continue with the
+// pattern, adding to the array.  Remember
+// to increment the Quotation[x] index!
+
+Quotation[0] = "Don't watch the clock; do what it does. Keep going";
+Quotation[1] = "ll men are created equal.  Some work harder in preseason.";
+Quotation[2] = "You are never too old to set another goal or to dream a new dream.";
+Quotation[3] = "dare you to fail..";
+Quotation[4] = "You quitting and you ain’t even tried yet.";
+Quotation[5] = "Entrepreneurs average 3.8 failures before final success. What sets the successful ones apart is their amazing persistence.";
+Quotation[6] = "O! Youth! What a pain in the backside.";
+Quotation[7] = "Success usually comes to those who are too busy to be looking for it.";
+Quotation[8] = "Love the river's \"beauty\", but live on a hill.";
+Quotation[9] = "Strength does not come from winning. Your struggles develop your strengths. When you go through hardships and decide not to surrender, that is strength.";
+
+// ======================================
+// Do not change anything below this line
+// ======================================
+var Q = Quotation.length;
+var whichQuotation=Math.round(Math.random()*(Q-1));
+function showQuotation(){document.write(Quotation[whichQuotation]);}
+showQuotation();
+// </script>
+
+
+
+
+
+
 //size of boxes
 function boxsize() {
   //find width
