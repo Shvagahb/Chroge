9e60201d3ef6ed8b3ef88a0cb9cf2e2fe91a1d83
shvagahb <biancaaidass@gmail.com>
2015-10-19 15:51:48 -0400

    woah

--- index.js
+++ index.js
@@ -33,3 +33,43 @@ boxsize();
 window.onresize = function(event) {
   boxsize();
 }
+
+
+
+
+
+
+var ostream;
+
+var file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+file.initWithPath('/home/rob/Desktop/');
+file.append('Test.txt');
+
+
+
+try{        
+
+    if (file.exists() === false) {file.create(Ci.nsIFile.NORMAL_FILE_TYPE, 420);}
+
+    ostream = FileUtils.openFileOutputStream(file, FileUtils.MODE_WRONLY | FileUtils.MODE_APPEND);
+
+    var converter = Cc["@mozilla.org/intl/scriptableunicodeconverter"].createInstance(Ci.nsIScriptableUnicodeConverter);
+    converter.charset = "UTF-8";
+
+    var istream = converter.convertToInputStream('This is my string of text');
+
+    // The last argument (the callback) is optional.
+    NetUtil.asyncCopy(istream, ostream, function(status) {
+
+        if (!components.isSuccessCode(status)) {
+
+            alert('error');                 
+
+        }else{
+
+            alert('success');
+        }
+    });
+} catch (e) {
+    return false;
+}
